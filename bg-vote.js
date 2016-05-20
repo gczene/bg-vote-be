10a40bf2c90c28d6f0aaf793852a69a8b06d267e
gczene <gabor@czene.co.uk>
2016-05-20 14:22:30 +0100

    CORS

--- bg-vote.js
+++ bg-vote.js
@@ -7,6 +7,12 @@ var bodyParser = require('body-parser');
 var db = require('./lib/monk');
 var releases = require('./lib/releases');
 
+
+app.get('/', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
